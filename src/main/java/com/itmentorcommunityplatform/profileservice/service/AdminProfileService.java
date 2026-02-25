8e00d2944bd1f29167a2a6f6e714c7f02e3e2c04
Misha Pivovarov <pivovarov.pr@gmail.com>
2026-02-26 00:16:46 +0300

    refactor: Заинлайнил return, убрал временную переменную items и вернул private метод в класс

--- src/main/java/com/itmentorcommunityplatform/profileservice/service/AdminProfileService.java
+++ src/main/java/com/itmentorcommunityplatform/profileservice/service/AdminProfileService.java
@@ -1,9 +1,14 @@
 package com.itmentorcommunityplatform.profileservice.service;
 
+import com.itmentorcommunityplatform.profileservice.client.AuthServiceClient;
 import com.itmentorcommunityplatform.profileservice.domain.Profile;
+import com.itmentorcommunityplatform.profileservice.domain.ProfileDetail;
+import com.itmentorcommunityplatform.profileservice.domain.type.Role;
+import com.itmentorcommunityplatform.profileservice.dto.external.UserWithRolesResponseDto;
 import com.itmentorcommunityplatform.profileservice.dto.response.AllProfilesPaginatedResponseDto;
+import com.itmentorcommunityplatform.profileservice.dto.response.ProfileDetailsResponseDto;
 import com.itmentorcommunityplatform.profileservice.dto.response.ProfileWithRolesResponseDto;
-import com.itmentorcommunityplatform.profileservice.exception.BadRequestException;
+import com.itmentorcommunityplatform.profileservice.exception.InvalidPaginationException;
 import com.itmentorcommunityplatform.profileservice.repository.ProfileRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +16,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -19,6 +25,7 @@ public class AdminProfileService {
 
     private final ProfileRepository profileRepository;
     private final ProfileHelperService profileHelperService;
+    private final AuthServiceClient authServiceClient;
 
     public AllProfilesPaginatedResponseDto getAllProfiles(int pageSize,
                                                           int pageNumber,
@@ -53,17 +60,51 @@ public class AdminProfileService {
         totalPageCount = Math.max((int) Math.ceil((double) allProfilesCount / pageSize), 1);
 
         if (pageNumber > totalPageCount) {
-            throw new BadRequestException("Page number is greater than total page count");
+            throw new InvalidPaginationException("Page number is greater than total page count");
         }
 
-        List<ProfileWithRolesResponseDto> items;
-
         try {
-            items = profileHelperService.enrichProfilesWithRoles(allProfilesPaginated);
+            return new AllProfilesPaginatedResponseDto(
+                    allProfilesCount,
+                    totalPageCount,
+                    allProfilesPaginated.size(),
+                    pageNumber,
+                    enrichProfilesWithRoles(allProfilesPaginated)
+                    );
         } catch (Exception ex) {
             log.error("All attempts to fetch user roles failed. Reason: {}", ex.getMessage());
             throw ex;
         }
-        return new AllProfilesPaginatedResponseDto(allProfilesCount, totalPageCount, allProfilesPaginated.size(), pageNumber, items);
+    }
+
+    private List<ProfileWithRolesResponseDto> enrichProfilesWithRoles(List<Profile> profiles) {
+        if (profiles == null || profiles.isEmpty()) {
+            return List.of();
+        }
+
+        List<Long> telegramIds = profiles.stream()
+                .map(Profile::getTelegramUserId)
+                .toList();
+
+        List<UserWithRolesResponseDto> userRoles = authServiceClient.getAllUsers(telegramIds);
+
+        Map<Long, List<Role>> rolesById = userRoles.stream()
+                .collect(Collectors.toMap(
+                        UserWithRolesResponseDto::telegramUserId,
+                        user -> user.roles().stream()
+                                .map(role -> Role.valueOf(role.toUpperCase()))
+                                .toList()
+                ));
+
+        return profiles.stream()
+                .map(profile -> new ProfileWithRolesResponseDto(
+                        profile.getId(),
+                        new ProfileDetailsResponseDto(profile.getDetails().stream()
+                                .collect(Collectors.toMap(
+                                        ProfileDetail::getDetailName,
+                                        ProfileDetail::getDetailValue
+                                ))),
+                        rolesById.getOrDefault(profile.getTelegramUserId(), List.of())
+                )).toList();
     }
 }
